fd609520e6f307cb72e98c8805ac843d3588004a
TheZavitaev <o.zavitaev@gmail.com>
2020-09-22 12:36:28 +0300

    added tasks

---
+++ 2 lvl/Neighbours.py
@@ -0,0 +1,7 @@
+m = int(input())
+n = int(input())
+matrix = [[int(j) for j in input().split()] for i in range(m)]
+x = int(input())
+y = int(input())
+
+print(matrix)
---
+++ 3 lvl/Polish_notation.py
@@ -0,0 +1,44 @@
+import re
+
+OPERATORS = {
+    '+': float.__add__,
+    '-': float.__sub__,
+    '*': float.__mul__,
+    '/': float.__truediv__,
+    '%': float.__mod__,
+    '^': float.__pow__,
+}
+
+
+def reversed_polish_notation(expr):
+    ops = OPERATORS.keys()
+    stack = []
+
+    for atom in re.split(r'\s+', expr):
+        try:
+            atom = float(atom)
+            stack.append(atom)
+        except ValueError:
+            for oper in atom:
+                if oper not in ops:
+                    continue
+                try:
+                    oper2 = stack.pop()
+                    oper1 = stack.pop()
+                except IndexError:
+                    raise
+
+                try:
+                    oper = OPERATORS[oper](oper1, oper2)
+                except ZeroDivisionError:
+                    raise
+
+                stack.append(oper)
+
+    if len(stack) != 1:
+        raise
+
+    return stack.pop()
+
+
+print(int(reversed_polish_notation(input())))
